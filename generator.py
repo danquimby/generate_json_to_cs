0d8e59616dd272015187275e1ccefe5821cadf4f
danquimby <danquimby@gmail.com>
2023-01-08 12:17:31 +0300

    add generate py class

--- generator.py
+++ generator.py
@@ -3,13 +3,25 @@ import os
 from os import walk
 from os.path import join
 
-tmp = """using UnityEngine;
+tmp_csharp = """using UnityEngine;
 
 [System.Serializable]
 public class %s
 {
 %s
 }
+"""
+
+tmp_python = """from pydantic import BaseModel, Field
+
+
+class %s(BaseModel):
+%s
+"""
+
+"""
+class UserModel(BaseModel):
+    username: str = Field('user', example='')
 """
 
 path_dir = os.path.abspath(os.path.dirname(__file__))
@@ -27,11 +39,9 @@ def gen_data(files: str):
             yield (file, json.loads(f.read()))
 
 
-def save_csharp(data: str, filename: str, path_folder_result):
-    cs_filename = f"{filename.split('.')[0]}_model.cs"
-    with open(join(path_folder_result, cs_filename), 'w') as f:
+def _save(data: str, filename: str, path_folder_result):
+    with open(join(path_folder_result, filename), 'w') as f:
         f.write(data)
-
 
 def generate_csharp(data: dict, filename: str):
     class_name = f'{filename.split(".")[0].capitalize()}Model'
@@ -48,10 +58,29 @@ def generate_csharp(data: dict, filename: str):
         lines.append(f'\tpublic {t} {k}; //example {v} \n')
     # remove last character
     lines[-1] = lines[-1][:-1]
-    return tmp % (class_name, ''.join(lines))
+    return tmp_csharp % (class_name, ''.join(lines))
+
+def generate_python(data: dict, filename: str):
+    # %s: %s = Field('%s', example='%s')
+    class_name = f'{filename.split(".")[0].capitalize()}Model'
+    lines = []
+    for k, v in data.items():
+        t = 'None'
+        if isinstance(v, int):
+            t = 'int'
+        elif isinstance(v, float):
+            t = 'float'
+        elif isinstance(v, str):
+            t = 'str'
+        lines.append(f"\t{k}: {t} = Field('{v}')\n")
+    # remove last character
+    lines[-1] = lines[-1][:-1]
+    return tmp_python % (class_name, ''.join(lines))
 
 
 def run(source_folder: str, result_folder: str):
     for file, data in gen_data(get_source_files(source_folder)):
-        get_class = generate_csharp(data, file)
-        save_csharp(get_class, file, result_folder)
+        generate_cs_class = generate_csharp(data, file)
+        _save(generate_cs_class, f"{file.split('.')[0]}_model.cs", result_folder)
+        generate_py_class = generate_python(data, file)
+        _save(generate_py_class, f"{file.split('.')[0]}_model.py", result_folder)
